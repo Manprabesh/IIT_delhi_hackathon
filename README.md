4cc59ff993669f626094121fecb54d3666bdf52b
Manprabesh <billionaireboruah31@gmail.com>
2025-01-11 23:07:25 +0530

    updated

--- README.md
+++
@@ -1,7 +0,0 @@
-# IIT-Delhi-Hackathon
-#
-
-# Day 1
-### Created basics of user signup routes and models ###
-#
-
--- routes/user_signup_route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const user_signup=express.Router()
-import { user_signup_controller } from '../controller/user_signup_controller.js'
-
-user_signup.post('/user/signup',user_signup_controller)
-
-export{
-    user_signup
-}
