1f9a24d73333b55f12d4e7014a3ffb66b88934d6
Manprabesh <billionaireboruah31@gmail.com>
2025-01-16 22:05:36 +0530

    Added filter for downloading product on product category, name, min and max price

--- seller_backend/controllers/product_download_controller.js
+++ seller_backend/controllers/product_download_controller.js
@@ -28,17 +28,43 @@ const product_download_controller = async (req, res) => {
 
 
 //For seller to download their product images
-const product_download_controller_by_email=async(req,res)=>{
-    const seller_email=res.user_data.seller_email
+const product_download_controller_by_email = async (req, res) => {
+    const seller_email = res.user_data.seller_email
     console.log(seller_email)
     // console.log(seller_email)
     // res.json({"email":seller_email})
-   const seller_products=await  Authentication_process_1_model.findOne({seller_email:seller_email}).populate("seller_product")
+    const seller_products = await Authentication_process_1_model.findOne({ seller_email: seller_email }).populate("seller_product")
 
-   console.log("From product_download_controller_by_email",seller_products)
+    console.log("From product_download_controller_by_email", seller_products)
 
 
-   return res.status(200).json({"seller products": seller_products})
+    return res.status(200).json({ "seller products": seller_products })
 }
 
-export  {product_download_controller,product_download_controller_by_email}
+const product_download_by_category = async (req, res) => {
+    const { product_category, min_price, max_price, product_name } = req.body
+
+    //Add logic for verifying incoming data -> if(product_name){}
+
+    console.log(typeof (min_price))
+
+    let min_num = Number(min_price)
+    let max_num = Number(max_price)
+    console.log(typeof (num))
+
+
+    let product_obj = {
+        product_name:{ $regex: product_name, $options: "i" },
+        product_category: { $regex: product_category, $options: "i" },
+        product_price: { $gte: min_num, $lte: max_num }
+    }
+    console.log(product_obj)
+
+    console.log(product_obj.product_price.$gte)
+    console.log(product_obj.product_price.$lte)
+
+    const product_list = await Product.find(product_obj)
+    res.json(product_list)
+}
+
+export { product_download_controller, product_download_controller_by_email, product_download_by_category }
